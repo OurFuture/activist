1ae43314c776d57185f1cbc7328c87bb7d78f58a
IanLondon <ianlondondu@gmail.com>
2016-11-19 01:16:47 -0500

    more OrganizationDetails

--- src/components/OrganizationDetail.js
+++ src/components/OrganizationDetail.js
@@ -11,13 +11,26 @@ class OrganizationDetail extends Component {
 	}
 
 	render(){
+		// find the first matching slug in the state
 		const slug = this.props.params.slug
 		const org = this.props.organizations.list.find((organization) => {return organization.slug == slug})
+		if (org == undefined) {
+			return <h1>Unhandled error: Bad link, or org with given slug not in state...</h1>
+		}
 		return (
 			<div>
-				<h1>Is this code a terrible hack?</h1>
-				<p>OrganizationDetail page for org: {org.name} </p>
-				<p>All the deets: {JSON.stringify(org)}</p>
+				<h1>Is this OrganizationDetail code a terrible hack?</h1>
+				<h1>{org.name}</h1>
+				<div>
+					<strong>Description</strong>
+					<p>{org.description}</p>
+				</div>
+				<div>
+					<strong>Address</strong>
+					<p>{org.address.street}</p>
+					<p>{org.address.state}, {org.address.city}</p>
+				</div>
+				<small>All the deets: {JSON.stringify(org)}</small>
 			</div>
 
 		)
@@ -26,7 +39,6 @@ class OrganizationDetail extends Component {
 }
 
 const stateToProps = (state) => {
-	console.log('woo', state.organization)
 	return {
 		organizations: state.organization
 	}
