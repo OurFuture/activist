565712ad11e51867c0c7bf2f0aaa86326d298602
IanLondon <ianlondondu@gmail.com>
2016-11-22 03:07:29 -0500

    Map tweaks

--- src/components/Map.js
+++ src/components/Map.js
@@ -28,24 +28,27 @@ class Map extends Component {
 		var markers = null
 		if (this.props.organizations != null){
 			markers = this.props.organizations.map(org => {
-				org['defaultAnimation'] = this.props.animation
-				// marker['icon'] = marker.image+'=s60-c'
-				org['position'] = {
+				org.defaultAnimation = this.props.animation
+				// org['icon'] = org.image+'=s60-c' //TODO: org/event icons on map
+				org.position = {
 					lat: org.geo[0],
 					lng: org.geo[1]
-				}
+				},
+				org.opacity = (this.props.active_org === org.id) ? 1.0 : 0.75 //TODO: up opacity when selected
 
 		        return (
 		            <Marker
 		            	key={org.id}
 		            	clickable={true}
 		            	// icon={org.icon}
-		            	label={org.name + 'label'}
-		            	title={org.name + 'title'}
+		            	label={org.name} // displayed as text over the icon
+		            	title={org.name} //???
 		            	onClick={this.handleMarkerClick.bind(this, org)}
 		            	{...org} />
 		        )
 			})
+		} else {
+			console.warn("Map found no organizations!")
 		}
 
 		const mapContainer = <div id={this.props.id} className={this.props.className}></div>
@@ -76,45 +79,6 @@ class Map extends Component {
 
 export default Map
 
-// import React, { Component } from 'react'
-// import { connect } from 'react-redux'
-//
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   console.log(browserHasGeolocation ? "got geo: " + pos : "no geo")
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(browserHasGeolocation ?
-//     'Error: The Geolocation service failed.' :
-//     'Error: Your browser doesn\'t support geolocation.');
-// }
-//
-// class GeolocationMap extends Component {
-//   constructor(props){
-//     super()
-//
-//     if (!props.google) {
-//       console.error("No google object in props!")
-//     }
-//
-//     this.state = {
-//       map: {},
-//       infoWindow: {},
-//       google: props.google,
-//       markers: []
-//     }
-//   }
-//
-//   componentDidMount() {
-//     this.state.map = new this.state.google.maps.Map(document.getElementById(this.props.id), {
-//       center: {
-//         lat: -34.397,
-//         lng: 150.644
-//       },
-//       zoom: 14
-//     })
-//     this.state.infoWindow = new this.state.google.maps.InfoWindow({
-//       map: this.state.map
-//     })
-//
 //     var _this = this //TODO: HACK! Use bind.
 //     // Try HTML5 geolocation.
 //     if (navigator.geolocation) {
@@ -123,33 +87,3 @@ export default Map
 //           lat: position.coords.latitude,
 //           lng: position.coords.longitude
 //         }
-//         // "You are here" info
-//         _this.state.infoWindow.setPosition(pos);
-//         _this.state.infoWindow.setContent('You are here.');
-//         // actually move the map
-//         _this.state.map.setCenter(pos);
-//       }, function() {
-//         handleLocationError(true, _this.state.infoWindow, _this.state.map.getCenter());
-//       });
-//     } else {
-//       // Browser doesn't support Geolocation
-//       handleLocationError(false, _this.state.infoWindow, _this.state.map.getCenter());
-//     }
-//
-//     // example of adding a marker
-//     this.state.markers.push(new this.state.google.maps.Marker({
-//       position: {lat:40.747154, lng:-73.948426},
-//       title: "Test marker",
-//       map: this.state.map
-//     }))
-//   }
-//
-//   render() {
-//     return (
-//       <div id={this.props.id} className={this.props.className}></div>
-//     )
-//   }
-//
-// }
-//
-// export default GeolocationMap
