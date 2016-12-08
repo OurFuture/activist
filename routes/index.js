c1503fbd540f089d755467d483a15c9b9bdf76a6
IanLondon <ianlondondu@gmail.com>
2016-12-07 22:41:47 -0500

    remove deprecated /:page route

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Get Involved.' })
 })
 
-router.get('/:page', function(req, res, next) {
-	if (req.params.page == 'sendgrid'){
-		next()
-		return
-	}
-
-    res.render(req.params.page, { title: 'TODO Name Page' })
-
-})
+// router.get('/:page', function(req, res, next) {
+// 	if (req.params.page == 'sendgrid'){
+// 		next()
+// 		return
+// 	}
+//
+//     res.render(req.params.page, { title: 'TODO Name Page' })
+//
+// })
 
 module.exports = router
